1e91f8bb2fbaeb3b821b55bb2c202986e4873c3e
Rajathare <raj.athare2000@gmail.com>
2024-05-13 17:43:47 +0530

    Stop Watch Web Application

---
+++ README.md
@@ -0,0 +1,4 @@
+# Stop Watch Web Application
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/RAJ-Athare-INDIA/pen/oNRgmeo](https://codepen.io/RAJ-Athare-INDIA/pen/oNRgmeo).
+
---
+++ script.js
@@ -0,0 +1,6 @@
+<section id="hero">
+  <img src="gym-image.jpg" alt="Gym Image">
+  <h1>Sky Gym - Your Fitness Destination</h1>
+  <p>Your journey to fitness starts here.</p>
+  <a href="#contact" class="cta-button">Join Now</a>
+</section>
